09cc4f91a2c9376a880e3984fdcc4f3a202b066e
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2026-03-15 19:50:13 +0000

    feat: First commit, doesn't update but compiles

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::cell::Cell;
+use std::rc::Rc;
+
+use glib::clone;
+use gtk::{glib, Application, Button, Label, Orientation};
+use gtk::{prelude::*, ApplicationWindow};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+
+// Widget Gallery: https://docs.gtk.org/gtk4/visual_index.html
+// Button docs: https://docs.gtk.org/gtk4/class.Button.html#implements
+
+fn build_ui(app: &Application) {
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let button_decrease = Button::builder()
+        .label("Decrease")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let number = Rc::new(Cell::new(0));
+
+    let label = Label::builder().build();
+    label.set_label(&format!("Number: {}", number.get()));
+
+    button_increase.connect_clicked(clone!(
+        #[weak]
+        number,
+        #[weak]
+        label,
+        move |_| {
+            let n = number.get() + 1;
+            number.set(n);
+            eprintln!("N: {n}");
+            label.set_label(&format!("Number: {n}"));
+        }
+    ));
+
+    // VBox
+    let vbox = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+
+    vbox.append(&button_increase);
+    vbox.append(&label);
+    vbox.append(&button_decrease);
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&vbox)
+        .build();
+
+    window.present()
+}
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
